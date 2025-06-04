539bc736f6a5d0ff24d4ccf1f15f8c10bb880a89
motazsamoud <90939560+motazsamoud@users.noreply.github.com>
2025-06-04 03:37:47 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar: React.FC = () => {
     <nav className="fixed top-0 left-0 w-full text-white p-4 flex justify-between items-center z-20">
       <div className="flex items-center">
         <Link
-          to="/Portfolio"
+          to="/motaz-sammoud-portfolio"
           className="text-lg font-semibold hover:text-pink-400 select-none"
         >
           MyPortfolio
@@ -25,20 +25,20 @@ const Navbar: React.FC = () => {
       </div>
 
       <div className="md:flex space-x-4 hidden select-none">
-        <Link to="/Portfolio" className="hover:text-pink-400">
+        <Link to="/motaz-sammoud-portfolio" className="hover:text-pink-400">
           Home
         </Link>
-        <Link to="/Portfolio/experience" className="hover:text-pink-400">
+        <Link to="/motaz-sammoud-portfolio/experience" className="hover:text-pink-400">
           Experience
         </Link>
 
-        <Link to="/Portfolio/education" className="hover:text-purple-400">
+        <Link to="/motaz-sammoud-portfolio/education" className="hover:text-purple-400">
           Education
         </Link>
-        <Link to="/Portfolio/skills" className="hover:text-blue-400">
+        <Link to="/motaz-sammoud-portfolio/skills" className="hover:text-blue-400">
           Skills
         </Link>
-        <Link to="/Portfolio/contact" className="hover:text-blue-400">
+        <Link to="/motaz-sammoud-portfolio/contact" className="hover:text-blue-400">
           Contact
         </Link>
       </div>
@@ -57,14 +57,14 @@ const Navbar: React.FC = () => {
         <div className="flex justify-center items-center h-full">
           <div className="space-y-4 text-center text-white">
             <Link
-              to="/Portfolio"
+              to="/motaz-sammoud-portfolio"
               className="hover:text-pink-300 block select-none"
               onClick={closeMenu}
             >
               Home
             </Link>
             <Link
-              to="/Portfolio/experience"
+              to="/motaz-sammoud-portfolio/experience"
               className="hover:text-pink-300 block select-none"
               onClick={closeMenu}
             >
@@ -72,21 +72,21 @@ const Navbar: React.FC = () => {
             </Link>
 
             <Link
-              to="/Portfolio/education"
+              to="/motaz-sammoud-portfolio/education"
               className="hover:text-pink-300 block select-none"
               onClick={closeMenu}
             >
               Education
             </Link>
             <Link
-              to="/Portfolio/skills"
+              to="/motaz-sammoud-portfolio/skills"
               className="hover:text-pink-300 block select-none"
               onClick={closeMenu}
             >
               Skills
             </Link>
             <Link
-              to="/Portfolio/contact"
+              to="/motaz-sammoud-portfolio/contact"
               className="hover:text-pink-300 block select-none"
               onClick={closeMenu}
             >
